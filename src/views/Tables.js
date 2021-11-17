bfa29584a2fd38f48e02994e363b1f0e897017b8
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-11-16 21:42:59 -0300

    exemplo de tabela

--- src/views/Tables.js
+++ src/views/Tables.js
@@ -20,6 +20,7 @@ import React from "react";
 
 // reactstrap components
 import {
+  Button,
   Card,
   CardHeader,
   CardBody,
@@ -37,60 +38,67 @@ function Tables() {
           <Col md="12">
             <Card>
               <CardHeader>
-                <CardTitle tag="h4">Simple Table</CardTitle>
+                <CardTitle tag="h4">Listagem de Encomendas</CardTitle>
               </CardHeader>
               <CardBody>
                 <Table responsive>
                   <thead className="text-primary">
                     <tr>
-                      <th>Name</th>
-                      <th>Country</th>
-                      <th>City</th>
-                      <th className="text-right">Salary</th>
+                      <th>Unidade</th>
+                      <th>Descrição</th>
+                      <th>Morador</th>
+                      <th>Status</th>
+                      <th className="text-right">Detalhe</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <td>Dakota Rice</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
-                      <td className="text-right">$36,738</td>
+                      <td>Armario X - compartimento Y</td>
+                      <td>Miojo</td>
+                      <td>Leandro A.</td>
+                      <td
+                        style={{
+                          color: "green",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Retirado
+                      </td>
+                      <td className="text-right">
+                        <Button color="primary">Ver mais</Button>
+                      </td>
                     </tr>
                     <tr>
-                      <td>Minerva Hooper</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
-                      <td className="text-right">$23,789</td>
+                      <td>Armario X - compartimento Y</td>
+                      <td>Miojo</td>
+                      <td>Leandro A.</td>
+                      <td
+                        style={{
+                          color: "orange",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Triagem
+                      </td>
+                      <td className="text-right">
+                        <Button color="primary">Ver mais</Button>
+                      </td>
                     </tr>
                     <tr>
-                      <td>Sage Rodriguez</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
-                      <td className="text-right">$56,142</td>
-                    </tr>
-                    <tr>
-                      <td>Philip Chaney</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
-                      <td className="text-right">$38,735</td>
-                    </tr>
-                    <tr>
-                      <td>Doris Greene</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                      <td className="text-right">$63,542</td>
-                    </tr>
-                    <tr>
-                      <td>Mason Porter</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
-                      <td className="text-right">$78,615</td>
-                    </tr>
-                    <tr>
-                      <td>Jon Porter</td>
-                      <td>Portugal</td>
-                      <td>Gloucester</td>
-                      <td className="text-right">$98,615</td>
+                      <td>Armario X - compartimento Y</td>
+                      <td>Miojo</td>
+                      <td>Leandro A.</td>
+                      <td
+                        style={{
+                          color: "red",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        Atraso
+                      </td>
+                      <td className="text-right">
+                        <Button color="primary">Ver mais</Button>
+                      </td>
                     </tr>
                   </tbody>
                 </Table>
