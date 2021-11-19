9668b40a6a12f1faef1685dd44f36fa38db54c9e
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-11-18 21:52:49 -0300

    feat: retornando listagem de encomendas

--- src/views/Tables.js
+++ src/views/Tables.js
@@ -1,22 +1,4 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // reactstrap components
 import {
@@ -30,7 +12,28 @@ import {
   Col,
 } from "reactstrap";
 
+import axios from "axios";
+
 function Tables() {
+  const [encomendas, setEncomendas] = useState([]);
+
+  useEffect(() => {
+    retornaListaDeEncomendas();
+  }, []);
+
+  const retornaListaDeEncomendas = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://127.0.0.1:8000/api/encomenda/lista"
+      );
+      setEncomendas(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  console.log(encomendas);
+
   return (
     <>
       <div className="content">
@@ -52,54 +55,24 @@ function Tables() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>Armario X - compartimento Y</td>
-                      <td>Miojo</td>
-                      <td>Leandro A.</td>
-                      <td
-                        style={{
-                          color: "green",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Retirado
-                      </td>
-                      <td className="text-right">
-                        <Button color="primary">Ver mais</Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Armario X - compartimento Y</td>
-                      <td>Miojo</td>
-                      <td>Leandro A.</td>
-                      <td
-                        style={{
-                          color: "orange",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Triagem
-                      </td>
-                      <td className="text-right">
-                        <Button color="primary">Ver mais</Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Armario X - compartimento Y</td>
-                      <td>Miojo</td>
-                      <td>Leandro A.</td>
-                      <td
-                        style={{
-                          color: "red",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Atraso
-                      </td>
-                      <td className="text-right">
-                        <Button color="primary">Ver mais</Button>
-                      </td>
-                    </tr>
+                    {encomendas?.map((encomenda) => (
+                      <tr key={encomenda?.id}>
+                        <td>{encomenda?.unidade}</td>
+                        <td>{encomenda?.descricao}</td>
+                        <td>{encomenda?.pessoa?.nome}</td>
+                        <td
+                          style={{
+                            color: "green",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Retirado
+                        </td>
+                        <td className="text-right">
+                          <Button color="primary">Ver mais</Button>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </Table>
               </CardBody>
