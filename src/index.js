e6ed14f3a481ba221e3a6a015b694486b60da9c8
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-11-29 23:28:11 -0300

    feat: ensure private and public routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.3.0";
 import "assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
-import AdminLayout from "layouts/Admin.js";
+import { Router } from "./router";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Redirect to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { PrivateRoute, PublicRoute } from "./components/Routes";
+
+export const Router = () => {
+  //  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PublicRoute path="/login" isAuthenticated={false}>
+          <div>usuário não autenticado</div>
+        </PublicRoute>
+        <PrivateRoute path="/dashboard" isAuthenticated={true} />
+        <Route path="*">
+          <div>
+            <h1>Página não encontrada</h1>
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
