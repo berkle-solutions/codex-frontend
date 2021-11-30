36e15e7a3ec82768b4159ca11d2d839dac9e8583
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-11-29 22:33:48 -0300

    feat: remove comments about creative tim

--- src/views/Triagem.js
+++ src/views/Triagem.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
 // reactstrap components
@@ -60,8 +42,7 @@ function Triagem() {
                           color: "green",
                           fontWeight: "bold",
                         }}
-                      >
-                      </td>
+                      ></td>
                       <td className="text-right">
                         <Button color="primary">Detalhes</Button>
                       </td>
@@ -75,8 +56,7 @@ function Triagem() {
                           color: "orange",
                           fontWeight: "bold",
                         }}
-                      >
-                      </td>
+                      ></td>
                       <td className="text-right">
                         <Button color="primary">Detalhes</Button>
                       </td>
@@ -90,8 +70,7 @@ function Triagem() {
                           color: "red",
                           fontWeight: "bold",
                         }}
-                      >
-                      </td>
+                      ></td>
                       <td className="text-right">
                         <Button color="primary">Detalhes</Button>
                       </td>
