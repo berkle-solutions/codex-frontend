7671622bfdd6b0a0e5a863a64f1683748c6d990f
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-12-10 20:05:47 -0300

    feat: simplify dash to bigNumber

--- src/views/PrivateViews/Dashboard.js
+++ src/views/PrivateViews/Dashboard.js
@@ -1,24 +1,63 @@
-import React from "react";
-// react plugin used to create charts
-import { Line, Pie } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
 // reactstrap components
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Row,
-  Col,
-} from "reactstrap";
-// core components
-import {
-  dashboard24HoursPerformanceChart,
-  dashboardEmailStatisticsChart,
-  dashboardNASDAQChart,
-} from "variables/charts.js";
+import { Card, CardBody, CardTitle, Row, Col } from "reactstrap";
+// services
+import { getEncomendas } from "../../services/codex";
 
 function Dashboard() {
+  const [encomendas, setEncomendas] = useState([]);
+  const [bigNumbers, setBigNumbers] = useState({
+    triagem: 0,
+    estoque: 0,
+    retirado: 0,
+    atrasado: 0,
+  });
+
+  const retornaDadosBigNumbers = (encomendas) => {
+    let triagem = 0;
+    let estoque = 0;
+    let retirado = 0;
+    let atrasado = 0;
+
+    encomendas?.forEach((encomenda) => {
+      if (encomenda?.status_fila?.descricao === "Triagem") {
+        triagem = triagem + 1;
+      }
+      if (encomenda?.status_fila?.descricao === "Em Estoque") {
+        estoque = estoque + 1;
+      }
+      if (encomenda?.status_fila?.descricao === "Retirado") {
+        retirado = retirado + 1;
+      }
+      if (encomenda?.status_fila?.descricao === "Em Atraso") {
+        atrasado = atrasado + 1;
+      }
+    });
+
+    setBigNumbers({
+      triagem,
+      estoque,
+      retirado,
+      atrasado,
+    });
+  };
+
+  const retornaListaDeEncomendas = async () => {
+    try {
+      const response = await getEncomendas();
+      setEncomendas(response);
+      retornaDadosBigNumbers(response);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    retornaListaDeEncomendas();
+  }, []);
+
+  console.log(encomendas);
+
   return (
     <>
       <div className="content">
@@ -29,24 +68,18 @@ function Dashboard() {
                 <Row>
                   <Col md="4" xs="5">
                     <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-globe text-warning" />
+                      <i className="nc-icon nc-app text-warning" />
                     </div>
                   </Col>
                   <Col md="8" xs="7">
                     <div className="numbers">
-                      <p className="card-category">Capacity</p>
-                      <CardTitle tag="p">150GB</CardTitle>
+                      <p className="card-category">Triagem</p>
+                      <CardTitle tag="p">{bigNumbers.triagem}</CardTitle>
                       <p />
                     </div>
                   </Col>
                 </Row>
               </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <i className="fas fa-sync-alt" /> Update Now
-                </div>
-              </CardFooter>
             </Card>
           </Col>
           <Col lg="3" md="6" sm="6">
@@ -55,24 +88,18 @@ function Dashboard() {
                 <Row>
                   <Col md="4" xs="5">
                     <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-money-coins text-success" />
+                      <i className="nc-icon nc-box-2 text-success" />
                     </div>
                   </Col>
                   <Col md="8" xs="7">
                     <div className="numbers">
-                      <p className="card-category">Revenue</p>
-                      <CardTitle tag="p">$ 1,345</CardTitle>
+                      <p className="card-category">Estoque</p>
+                      <CardTitle tag="p">{bigNumbers.estoque}</CardTitle>
                       <p />
                     </div>
                   </Col>
                 </Row>
               </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <i className="far fa-calendar" /> Last day
-                </div>
-              </CardFooter>
             </Card>
           </Col>
           <Col lg="3" md="6" sm="6">
@@ -81,24 +108,18 @@ function Dashboard() {
                 <Row>
                   <Col md="4" xs="5">
                     <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-vector text-danger" />
+                      <i className="nc-icon nc-single-02 text-success" />
                     </div>
                   </Col>
                   <Col md="8" xs="7">
                     <div className="numbers">
-                      <p className="card-category">Errors</p>
-                      <CardTitle tag="p">23</CardTitle>
+                      <p className="card-category">Retirado</p>
+                      <CardTitle tag="p">{bigNumbers.retirado}</CardTitle>
                       <p />
                     </div>
                   </Col>
                 </Row>
               </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <i className="far fa-clock" /> In the last hour
-                </div>
-              </CardFooter>
             </Card>
           </Col>
           <Col lg="3" md="6" sm="6">
@@ -107,102 +128,18 @@ function Dashboard() {
                 <Row>
                   <Col md="4" xs="5">
                     <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-favourite-28 text-primary" />
+                      <i className="nc-icon nc-time-alarm text-danger" />
                     </div>
                   </Col>
                   <Col md="8" xs="7">
                     <div className="numbers">
-                      <p className="card-category">Followers</p>
-                      <CardTitle tag="p">+45K</CardTitle>
+                      <p className="card-category">Atraso</p>
+                      <CardTitle tag="p">{bigNumbers.atrasado}</CardTitle>
                       <p />
                     </div>
                   </Col>
                 </Row>
               </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <i className="fas fa-sync-alt" /> Update now
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="12">
-            <Card>
-              <CardHeader>
-                <CardTitle tag="h5">Users Behavior</CardTitle>
-                <p className="card-category">24 Hours performance</p>
-              </CardHeader>
-              <CardBody>
-                <Line
-                  data={dashboard24HoursPerformanceChart.data}
-                  options={dashboard24HoursPerformanceChart.options}
-                  width={400}
-                  height={100}
-                />
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <i className="fa fa-history" /> Updated 3 minutes ago
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="4">
-            <Card>
-              <CardHeader>
-                <CardTitle tag="h5">Email Statistics</CardTitle>
-                <p className="card-category">Last Campaign Performance</p>
-              </CardHeader>
-              <CardBody style={{ height: "266px" }}>
-                <Pie
-                  data={dashboardEmailStatisticsChart.data}
-                  options={dashboardEmailStatisticsChart.options}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="legend">
-                  <i className="fa fa-circle text-primary" /> Opened{" "}
-                  <i className="fa fa-circle text-warning" /> Read{" "}
-                  <i className="fa fa-circle text-danger" /> Deleted{" "}
-                  <i className="fa fa-circle text-gray" /> Unopened
-                </div>
-                <hr />
-                <div className="stats">
-                  <i className="fa fa-calendar" /> Number of emails sent
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col md="8">
-            <Card className="card-chart">
-              <CardHeader>
-                <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                <p className="card-category">Line Chart with Points</p>
-              </CardHeader>
-              <CardBody>
-                <Line
-                  data={dashboardNASDAQChart.data}
-                  options={dashboardNASDAQChart.options}
-                  width={400}
-                  height={100}
-                />
-              </CardBody>
-              <CardFooter>
-                <div className="chart-legend">
-                  <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                  <i className="fa fa-circle text-warning" /> BMW 5 Series
-                </div>
-                <hr />
-                <div className="card-stats">
-                  <i className="fa fa-check" /> Data information certified
-                </div>
-              </CardFooter>
             </Card>
           </Col>
         </Row>
